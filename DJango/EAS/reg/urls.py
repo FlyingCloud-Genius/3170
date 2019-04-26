54e9ea5620cb3a876f0f1d272854df9df936de58
FlyingCloud-Genius <116010264@link.cuhk.edu.cn>
2019-04-26 16:55:49 +0800

    blah

--- DJango/EAS/reg/urls.py
+++ DJango/EAS/reg/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [ 
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
-    
     path('index', views.index, name='index'),
     
 
