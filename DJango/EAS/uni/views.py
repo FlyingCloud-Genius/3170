96b1d4f985049d5b870886d5deffbd2d2fc697d4
FlyingCloud-Genius <116010264@link.cuhk.edu.cn>
2019-04-27 22:19:00 +0800

    uni page deal

--- DJango/EAS/uni/views.py
+++ DJango/EAS/uni/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
 
+from .models import University, UniOpenMajor
 # Create your views here.
 def profile(request):
-    pass
-    return render(request, 'uni/university-profile.html')
+    if request.method == "GET":
+        uni = University.objects.get(uni_email = "admissions@cuhk.edu.cn")
+        name = uni.uni_name
+        phone = uni.uni_phone
+        email = uni.uni_email
+        web = uni.uni_web
+        adress = uni.uni_address
+        # photo
+        return render_to_response('uni/university-profile.html', locals())
+    return HttpResponse(request)
