93d1ea8bb7c9eee88f38fc1e1f811e9ac93c9b09
Mochiheng <116010169@link.cuhk.edu.cn>
2019-04-29 03:45:11 +0800

    DJango V2.3

--- DJango/EAS/stu/urls.py
+++ DJango/EAS/stu/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [ 
     path('editor/<str:userID>', views.editor, name='personal-edit'),
     path('profile/<str:userID>', views.profile, name='personal-profile'),
-    path('application', views.application, name='application'),
-    path('exercise', views.exercise, name='exercise'),
-    path('enrollment', views.exams, name='tables-exams'),
-    path('grades', views.grades, name='tables-grades'),
+    path('application/<str:userID>', views.application, name='application'),
+    path('exercise/<str:userID>', views.exercise, name='exercise'),
+    path('enrollment/<str:userID>', views.exams, name='tables-exams'),
+    path('grades/<str:userID>', views.grades, name='tables-grades'),
 
 ]
